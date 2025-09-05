0f9ea539bf50d00a2328ae483fde8b964f3e97b8
Sina-Soroosh <sinasoroosh07@gmail.com>
2025-09-05 11:42:51 +0330

    feat : connect to db

---
+++ src/utils/db.ts
@@ -0,0 +1,32 @@
+import { Client } from "pg";
+
+let globalClient: Client | null = null;
+
+const connectToDB = async () => {
+  try {
+    globalClient = new Client({
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 5432,
+      database: process.env.DB_NAME,
+    });
+
+    await globalClient.connect();
+
+    console.log("CONNECT TO DB SUCCESSFULLY");
+  } catch (error) {
+    console.log("ERROR TO CONNECT TO DB : ", error);
+    globalClient = null;
+  }
+};
+
+const getClient = async () => {
+  if (!globalClient) {
+    await connectToDB();
+  }
+
+  return globalClient;
+};
+
+export default getClient;
